4faa9149e6d11de5ec35e0ea46cb1c7d48870cb1
jborrajorodriguez <jborrajorodriguez@victoria22.danielcastelao.org>
2017-10-25 09:26:58 +0200

    Clase Principal

---
+++ src/ejemplo/atributos/objectos/EjemploAtributosObjectos.java
@@ -0,0 +1,12 @@
+package ejemplo.atributos.objectos;
+/**
+ * @author jborrajorodriguez
+ */
+public class EjemploAtributosObjectos {
+
+    public static void main(String[] args) {
+       Conta conta=new Conta("millonetis","11111111111",100000000);
+        System.out.println(conta.toString());
+    }
+    
+}
